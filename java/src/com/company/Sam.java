ebe092e8edd00dea4790cb9aa271b19dbfc56a35
Ciprian Khlud <ckhlud@arxan.com>
2019-07-09 10:53:17 +0300

    local implementation working against 'input.sam'

---
+++ java/src/com/company/Sam.java
@@ -0,0 +1,11 @@
+/*
+	Copyright (c) 2018 by imec vzw, Leuven, Belgium. All rights reserverd.
+*/
+
+package com.company;
+
+
+public class Sam {
+  public SamHeader header;
+  public SamAlignment[] alignments;
+}
